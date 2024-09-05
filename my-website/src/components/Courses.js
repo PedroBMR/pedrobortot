390c1949a48db23f4d3d53ad6854706763cf4165
PedroBMR <pedroluizbmr@gmail.com>
2024-09-05 11:28:25 -0300

    Cursos Alura

--- my-website/src/components/Courses.js
+++ my-website/src/components/Courses.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './Courses.css';
-import { useTranslation } from 'react-i18next'; // Importa o hook de tradução
+import { useTranslation } from 'react-i18next';
 
 function Courses() {
-  const { t } = useTranslation(); // Acessa a função t para usar traduções
+  const { t } = useTranslation();
 
-  const courses = t('courses.list', { returnObjects: true }); // Carrega a lista de cursos traduzidos
+  const courses = t('courses.list', { returnObjects: true });
+  const aluraCertificates = t('courses.certificatesAlura', { returnObjects: true });
 
   return (
     <section id="courses" className="courses-section">
-      <h2>{t('courses.title')}</h2> {/* Título traduzido */}
+      <h2>{t('courses.title')}</h2>
+      <div className="certificates-container">
+        <a 
+          href={aluraCertificates.link} 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="certificate-link"
+        >
+          {aluraCertificates.text}
+        </a>
+      </div>
       <div className="courses-container">
         {courses.map((course, index) => (
           <div key={index} className="course-item">
