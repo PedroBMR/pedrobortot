6805d6e7c316920093e4eab88d33c1319feba719
Pedro Luiz Bortot Monteiro do Rosário <60971278+PedroBMR@users.noreply.github.com>
2025-08-14 14:13:08 -0300

    chore: add eslint, prettier, husky and lint-staged

---
+++ my-website/eslint.config.js
@@ -0,0 +1,25 @@
+import react from 'eslint-plugin-react';
+
+export default [
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        node: true
+      }
+    },
+    plugins: { react },
+    rules: {
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+];
